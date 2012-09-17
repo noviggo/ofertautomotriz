bad8be430a82b1de6e3a216c9e089f64fc56a68e
Flavia Ibañez <flavia.ibanez@gmail.com>
2012-09-17 16:01:38 -0300

    layouts

--- scripts/onload.js
+++ scripts/onload.js
@@ -9,8 +9,24 @@ var set_active_item_when_slide= function(){
   el = $('.pag-item[rel='+ number +']')[0]
   set_active_item(el);
 } 
+var set_active_item= function(element){
+  $el =  $(element);
+  $('#modalCarousel').carousel(parseInt($el.attr('rel')));
+  $el.parent().find('span').removeClass('active');
+  $el.addClass('active');
+}
+var set_active_item_when_slide= function(){
+  number = $('#modalCarousel div.item.active').attr('rel');
+  el = $('.pag-item[rel='+ number +']')[0]
+  set_active_item(el);
+}
 $(function(){
   
   $('#myCarousel').on('slid', function(){set_active_item_when_slide();})
   $('#myCarousel').carousel();
+  $('#modalCarousel').carousel();
+  $('#car-modal').modal('hide');
+  $('.slide-btn').click(function() {
+      $('.vehicle-details').animate({width: 'toggle'});
+  });
 })
